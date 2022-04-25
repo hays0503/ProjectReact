731786058689d00e1a6b0ae96147f7b94c9331f2
hays hays0503 <cthtufgbv@mail.ru>
2022-04-25 11:13:28 +0600

    delete debug log

--- src/Posts/PostList.js
+++ src/Posts/PostList.js
@@ -1,11 +1,6 @@
 import React from "react";
 import PostsItems from "./PostsItems";
 
-export function getPostItems(props, id) {
-  props.Item((id) => {
-    return <PostsItems id={id} key={id.id} />;
-  });
-}
 
 export default function PostList(props) {
   const postsElementRef = React.useRef(null);
